8b129cef754ad33458632361713a5b25a3545522
Aibek9320 <123246995+Aibek9320@users.noreply.github.com>
2023-02-03 19:13:47 +0600

    certificates problem

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
         items: 1,
         loop: true,
     });
+    var certSlider = $('#certificates-slider');
+    certSlider.owlCarousel({
+        items: 5,
+        margin: 30
+    })
+    $('.custom-next').click(function() {
+        certSlider.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.custom-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        certSlider.trigger('prev.owl.carousel', [300]);
+    })
   });
